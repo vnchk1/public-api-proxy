949ec1af13cc69a27793a78c28448247dde20d7c
Ivan Cherviakov <ivanchervyakov2905@gmail.com>
2025-07-07 18:33:39 +0300

    Initial commit of resty

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"github.com/go-resty/resty/v2"
+	"time"
+)
+
 func main() {
+	defaultBaseURL := "https://jsonplaceholder.typicode.com/"
+	client := resty.New()
+	client.SetBaseURL(defaultBaseURL).
+		SetTimeout(10*time.Second).
+		SetHeader("Content-Type", "application/json")
 
 }
