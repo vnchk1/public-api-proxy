c0d18dac880a5d051262cf264ad7c5c949e7e440
Ivan Cherviakov <ivanchervyakov2905@gmail.com>
2025-07-08 13:24:39 +0300

    resty client with only main.go

--- main.go
+++ main.go
@@ -1,15 +1,41 @@
 package main
 
 import (
+	"fmt"
 	"github.com/go-resty/resty/v2"
 	"time"
 )
 
+type Post struct {
+	ID     int64  `json:"id"`
+	Title  string `json:"title"`
+	Body   string `json:"body"`
+	UserID int64  `json:"userId"`
+}
+
 func main() {
-	defaultBaseURL := "https://jsonplaceholder.typicode.com/"
+	baseURL := "https://jsonplaceholder.typicode.com/"
 	client := resty.New()
-	client.SetBaseURL(defaultBaseURL).
+	client.SetBaseURL(baseURL).
 		SetTimeout(10*time.Second).
 		SetHeader("Content-Type", "application/json")
+	//get-запрос к posts/{id}
+	var post Post
+	postId := "1"
+	postPath := fmt.Sprintf("posts/%s", postId)
+	resp, err := client.R().SetResult(&post).Get(postPath)
 
+	if err != nil {
+		fmt.Printf("Sending request error: %v", err)
+		return
+	}
+
+	if resp.IsError() {
+		fmt.Printf("API error: %v %v", resp.Error(), resp.StatusCode())
+	}
+
+	fmt.Printf("Post: %v\n", post.ID)
+	fmt.Printf("Title: %v\n", post.Title)
+	fmt.Printf("Body: %v\n", post.Body)
+	fmt.Printf("UserID: %v\n", post.UserID)
 }
